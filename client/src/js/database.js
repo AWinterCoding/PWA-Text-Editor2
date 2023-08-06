af5f61ddc059a96f0f2dcaa1661a64f82afc1c10
Alexandra <awinterdrova@gmail.com>
2023-08-06 14:00:55 -0600

    final working build before heroku push

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// DONE?: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const textDB = await openDB('jate', 1);
+  const yz = textDB.transaction('jate', 'readwrite');
+  const store = yz.objectStore('jate');
+  const request = store.put({ jate: content});
+  const result = await request;
+  console.log("This stuff worked", result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const textDB = await openDB('jate', 1);
+
+  const yz = textDB.transaction('jate', 'readonly');
+  const store = yz.objectStore('jate');
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('this stuff worked', result);
+  return result;
+}
 
 initdb();
